932ecd826218a92c5070d8aaecd40d29da912adb
komronium <komronium@gmail.com>
2025-01-04 21:13:32 +0500

    Add set0 command

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -143,42 +143,58 @@ async def set0(message: Message):
         if message.from_user.id not in ADMINS:
             return
 
-        start_of_year = datetime(2025, 1, 1)
-        end_of_year = datetime(2025, 12, 31)
-
-
-        await bot.send_chat_action(message.chat.id, 'typing')
-        await sleep(0.2)
-        await message.answer('Calculating ...')
-
-        new_promos = await Promo.filter(date__gte=start_of_year, date__lte=end_of_year).order_by('date')
-        await message.answer('Yangi promo kodlar olindi')
-        await message.answer(str(len(new_promos)))
-        old_promos = await Promo.filter(date__lt=start_of_year).all()
-        await message.answer('Eski promo kodlar olindi')
-        await message.answer(str(len(old_promos)))
-        i = 1
-
-        for promo in old_promos:
-            promo.special_code = ''.join(choices(ascii_lowercase, k=6))
-            await promo.save()
-
-        await bot.send_chat_action(message.chat.id, 'typing')
-        await sleep(0.2)
-        await message.answer('The old promos have been successfully updated')
-
-        for promo in new_promos:
-            promo.special_code = str(i).zfill(6)
-            await promo.save()
-            i += 1
-
-        await bot.send_chat_action(message.chat.id, 'typing')
-        await sleep(0.2)
-        await message.answer("The new promos have been successfully updated.")
-        await message.answer('✅ DONE!')
+        # start_of_year = datetime(2025, 1, 1)
+        # end_of_year = datetime(2025, 12, 31)
+        #
+        #
+        # await bot.send_chat_action(message.chat.id, 'typing')
+        # await sleep(0.2)
+        # await message.answer('Calculating ...')
+        #
+        # new_promos = await Promo.filter(date__gte=start_of_year, date__lte=end_of_year).order_by('date')
+        # await message.answer('Yangi promo kodlar olindi')
+        # await message.answer(str(len(new_promos)))
+        # old_promos = await Promo.filter(date__lt=start_of_year).all()
+        # await message.answer('Eski promo kodlar olindi')
+        # await message.answer(str(len(old_promos)))
+        # i = 1
+        #
+        # for promo in old_promos:
+        #     promo.special_code = ''.join(choices(ascii_lowercase, k=6))
+        #     await promo.save()
+        #
+        # await bot.send_chat_action(message.chat.id, 'typing')
+        # await sleep(0.2)
+        # await message.answer('The old promos have been successfully updated')
+        #
+        # for promo in new_promos:
+        #     promo.special_code = str(i).zfill(6)
+        #     await promo.save()
+        #     i += 1
+        #
+        # await bot.send_chat_action(message.chat.id, 'typing')
+        # await sleep(0.2)
+        # await message.answer("The new promos have been successfully updated.")
+        # await message.answer('✅ DONE!')
         latest = await Promo.all().order_by('-date').first()
-        await message.answer(f'Date: {latest.date}')
+        await bot.send_chat_action(message.chat.id, 'typing')
+        await sleep(0.2)
+        await message.answer(f'<b>Latest</b>')
+        await message.answer(f'PromoDate: {latest.date}')
     except Exception as e:
         await bot.send_chat_action(message.chat.id, 'typing')
         await sleep(0.2)
         await message.answer(f"⁉️ Error: {str(e)}")
+
+
+@command_router.message(Command('latest'))
+async def get_latest(message: Message):
+    if message.from_user.id not in ADMINS:
+        return
+
+    latest = await Promo.all().order_by('-date').first()
+    await bot.send_chat_action(message.chat.id, 'typing')
+    await sleep(0.2)
+    await message.answer(f'<b>Latest</b>')
+    await message.answer((f'<b>Special Code:</b> {latest.special_code}\n'
+                          f'Date: {latest.date}'))
